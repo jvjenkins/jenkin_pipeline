630e65278986124960f95b18b1cd33c957f9940e
gowtham <gowtham.chinnahambi@treselle.com>
2016-10-24 17:14:49 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8081;
+
+// App
+const app = express();
+app.get('/', function (req, res) {
+  res.send('<html><body>Hi Folks .....how was the day !!!!!!!!</body></html>');
+});
+
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
