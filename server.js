e349037c9f94e28c6977f8c29d6307bbedf95bfb
gowtham <gowtham.chinnahambi@treselle.com>
2016-10-24 23:04:29 +0530

    Message updated

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = 8081;
 // App
 const app = express();
 app.get('/', function (req, res) {
-  res.send('<html><body>Continious integration with jenkin .....!!!!!!!!</body></html>');
+  res.send('<html><body>Hello World .....!!!!!!!!</body></html>');
 });
 
 app.listen(PORT);
